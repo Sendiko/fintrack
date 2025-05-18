de608913da0df6badbf077ce04dd10427f215dac
Sendiko <sarangtawon897@gmail.com>
2025-05-18 16:35:30 +0700

    feat: provide dependencies

--- composeApp/src/commonMain/kotlin/com/github/sendiko/fintrack/core/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/github/sendiko/fintrack/core/di/Modules.kt
@@ -1,5 +1,6 @@
 package com.github.sendiko.fintrack.core.di
 
+import com.github.sendiko.fintrack.core.preferences.PreferencesRepositoryImpl
 import com.github.sendiko.fintrack.splash.data.SplashRepositoryImpl
 import com.github.sendiko.fintrack.splash.presentation.SplashViewModel
 import org.koin.core.module.Module
@@ -11,5 +12,7 @@ expect val platformModule: Module
 val sharedModule = module {
     singleOf(::SplashRepositoryImpl)
 
+    factory { PreferencesRepositoryImpl(get()) }
+    factory { SplashRepositoryImpl(get()) }
     factory { SplashViewModel(get()) }
 }
