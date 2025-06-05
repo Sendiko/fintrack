61f642e2a4b3daf31977c7fb86c628de4b428554
Sendiko <sarangtawon897@gmail.com>
2025-06-05 13:12:24 +0700

    feat: implement register API call
    This commit implements the register API call in `KtorClient` and defines the corresponding function in the `ApiService` interface.
    
    - **KtorClient.kt**: Added the `register` function to make a POST request to the `/register` endpoint. It uses `safeCall` for error handling.
    - **ApiService.kt**: Added the `register` function definition.

--- composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/ApiService.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/ApiService.kt
@@ -1,5 +1,12 @@
 package id.my.sendiko.fintrack.core.network
 
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterRequest
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterResponse
+import id.my.sendiko.fintrack.core.network.utils.DataError
+import id.my.sendiko.fintrack.core.network.utils.Result
+
 interface ApiService {
+
+    suspend fun register(request: RegisterRequest): Result<RegisterResponse, DataError.Remote>
 
 }
--- composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/KtorClient.kt
@@ -1,9 +1,24 @@
 package id.my.sendiko.fintrack.core.network
 
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterRequest
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterResponse
+import id.my.sendiko.fintrack.core.network.utils.DataError
+import id.my.sendiko.fintrack.core.network.utils.Result
 import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+
+const val BASE_URL = "https://fintrack.sendiko.my.id"
 
 class KtorClient(
-    val client: HttpClient
+    private val client: HttpClient
 ): ApiService {
+    override suspend fun register(request: RegisterRequest): Result<RegisterResponse, DataError.Remote> {
+        return safeCall<RegisterResponse> {
+            client.post("$BASE_URL/register") {
+                setBody(request)
+            }
+        }
+    }
 
 }
