f622adb54e855603a04dd145e6e753389a0203e4
Sendiko <sarangtawon897@gmail.com>
2025-06-05 13:11:20 +0700

    feat: add User data class
    This commit introduces the `User` data class, which represents a user with a `userId`, `name`, and `email`.
    
    - **User.kt**: Defines the `User` data class and a companion object function `fromResponse` to convert a `UserData` DTO to a `User` object.

---
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/domain/User.kt
@@ -0,0 +1,19 @@
+package id.my.sendiko.fintrack.auth.domain
+
+import id.my.sendiko.fintrack.auth.register.data.dto.UserData
+
+data class User(
+    val userId: String,
+    val name: String,
+    val email: String,
+) {
+    companion object {
+        fun fromResponse(userData: UserData): User {
+            return User(
+                userId = userData.id,
+                name = userData.name,
+                email = userData.email
+            )
+        }
+    }
+}
