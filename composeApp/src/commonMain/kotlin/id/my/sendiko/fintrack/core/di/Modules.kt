ba3f64627c7f2eba278aaeaf020af4a3fe6819a5
Sendiko <sarangtawon897@gmail.com>
2025-06-05 13:14:14 +0700

    feat: add dependencies for authentication and registration
    This commit adds dependencies for authentication and registration features.
    
    - `HttpClientFactory` and `KtorClient` are now provided as singletons for network requests.
    - `RegisterRepository` is added as a singleton for handling registration data.
    - `RegisterViewModel` is added as a factory for managing the registration screen's logic.

--- composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/di/Modules.kt
@@ -1,18 +1,28 @@
 package id.my.sendiko.fintrack.core.di
 
+import id.my.sendiko.fintrack.auth.register.data.RegisterRepository
+import id.my.sendiko.fintrack.auth.register.presentation.RegisterViewModel
+import id.my.sendiko.fintrack.core.network.ApiService
+import id.my.sendiko.fintrack.core.network.HttpClientFactory
+import id.my.sendiko.fintrack.core.network.KtorClient
 import id.my.sendiko.fintrack.core.preferences.PreferencesRepositoryImpl
 import id.my.sendiko.fintrack.splash.data.SplashRepositoryImpl
 import id.my.sendiko.fintrack.splash.presentation.SplashViewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 expect val platformModule: Module
 
 val sharedModule = module {
+    single { HttpClientFactory.create(get()) }
+    singleOf(::KtorClient).bind<ApiService>()
     singleOf(::SplashRepositoryImpl)
+    singleOf(::RegisterRepository)
 
     factory { PreferencesRepositoryImpl(get()) }
     factory { SplashRepositoryImpl(get()) }
     factory { SplashViewModel(get()) }
+    factory { RegisterViewModel(get()) }
 }
