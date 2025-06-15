b1d7d9d408224615f611522a2a846dead5c57b9c
Sendiko <sarangtawon897@gmail.com>
2025-06-15 20:00:48 +0700

    feat: implement search user and update password API calls
    This commit adds the API call implementations for searching users by email and updating user passwords in `KtorClient`, and defines the corresponding functions in the `ApiService` interface.
    
    - **KtorClient.kt**:
        - Added the `searchUser` function to make a GET request to the `/users` endpoint with an email parameter.
        - Added the `updatePassword` function to make a PUT request to the `/user/change-password/{userId}` endpoint.
        - Both functions use `safeCall` for error handling.
    - **ApiService.kt**:
        - Added the `searchUser` function definition.
        - Added the `updatePassword` function definition.

--- composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/ApiService.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/core/network/ApiService.kt
@@ -1,5 +1,8 @@
 package id.my.sendiko.fintrack.core.network
 
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.ChangePasswordRequest
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.ChangePasswordResponse
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.SearchUserResponse
 import id.my.sendiko.fintrack.auth.login.data.dto.LoginRequest
 import id.my.sendiko.fintrack.auth.login.data.dto.LoginResponse
 import id.my.sendiko.fintrack.auth.register.data.dto.RegisterRequest
@@ -13,4 +16,7 @@ interface ApiService {
 
     suspend fun login(request: LoginRequest): Result<LoginResponse, DataError.Remote>
 
+    suspend fun searchUser(email: String): Result<SearchUserResponse, DataError.Remote>
+
+    suspend fun updatePassword(userId: String, request: ChangePasswordRequest): Result<ChangePasswordResponse, DataError.Remote>
 }
