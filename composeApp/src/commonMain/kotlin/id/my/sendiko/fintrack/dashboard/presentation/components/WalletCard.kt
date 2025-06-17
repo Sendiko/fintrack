611a62d78a029ace4114509b3e1911cacb9d7e5c
Sendiko <sarangtawon897@gmail.com>
2025-06-17 11:36:36 +0700

    feat: add WalletCard and AddWalletButton components
    This commit introduces two new UI components for the dashboard:
    
    - **WalletCard.kt**: A Composable function to display individual wallet information, including name, last four digits of the wallet number, and current balance formatted in Rupiah. It also includes a visibility toggle for the balance.
    - **AddWalletButton.kt**: An `OutlinedCard` Composable that serves as a button to add new wallets. It displays an add icon.
    - **formatRupiah.kt**: A utility extension function `Double.toRupiah()` to format a Double value into a Rupiah currency string (e.g., "Rp. 1.000.000").

---
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/dashboard/presentation/components/WalletCard.kt
@@ -0,0 +1,100 @@
+package id.my.sendiko.fintrack.dashboard.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import fintrack.composeapp.generated.resources.Res
+import fintrack.composeapp.generated.resources.password_visible
+import id.my.sendiko.fintrack.dashboard.data.wallets
+import id.my.sendiko.fintrack.dashboard.presentation.toRupiah
+import id.my.sendiko.fintrack.theme.FinTrackTheme
+import id.my.sendiko.fintrack.theme.secondaryBlue
+import id.my.sendiko.fintrack.theme.utilityWhite
+import id.my.sendiko.fintrack.wallet.domain.Wallet
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WalletCard(
+    modifier: Modifier = Modifier,
+    wallet: Wallet
+) {
+    Card(
+        modifier = modifier.width(256.dp)
+            .height(128.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = secondaryBlue,
+            contentColor = utilityWhite
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+
+            ) {
+                Text(
+                    text = wallet.name
+                )
+                Text(
+                    text = wallet.number.takeLast(4),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = wallet.amount.toRupiah(),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                IconButton(
+                    onClick = { }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Visibility,
+                        contentDescription = stringResource(Res.string.password_visible)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+private fun WalletCardPreview() {
+    FinTrackTheme {
+        WalletCard(wallet = wallets.first())
+    }
+}
