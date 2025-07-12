f6355751352fdc3dd67b32da3cf8dcb3cc10b1d3
Sendiko <sarangtawon897@gmail.com>
2025-07-12 14:37:19 +0700

    refactor: convert WalletType to enum class
    This commit refactors the `WalletType` sealed class to an enum class.
    
    - **WalletType.kt**: Changed `WalletType` from a `sealed class` to an `enum class` with values `CARD`, `E-MONEY`, and `CASH`.

--- composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/wallet/core/domain/WalletType.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/fintrack/wallet/core/domain/WalletType.kt
@@ -1,7 +1,7 @@
 package id.my.sendiko.fintrack.wallet.core.domain
 
-sealed class WalletType(name: String) {
-    class Card: WalletType("card")
-    class Emoney: WalletType("e-money")
-    class Cash: WalletType("cash")
+enum class WalletType(name: String) {
+    CARD("card"),
+    `E-MONEY`("e-money"),
+    CASH("cash")
 }
