d9a46023008530997c87ff2a9127a12e7b97b2a8
Sendiko <rizkysendiko7@gmail.com>
2026-06-05 21:29:32 +0700

    feat(auth): extract remote action to separate datasource

---
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/core/data/datasource/AuthRemoteDataSource.kt
@@ -0,0 +1,16 @@
+package id.my.sendiko.fintrack.auth.core.data.datasource
+
+import id.my.sendiko.fintrack.auth.login.data.dto.LoginRequest
+import id.my.sendiko.fintrack.auth.login.data.dto.LoginResponse
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterRequest
+import id.my.sendiko.fintrack.auth.register.data.dto.RegisterResponse
+import id.my.sendiko.fintrack.core.network.utils.DataError
+import id.my.sendiko.fintrack.core.network.utils.Result
+
+interface AuthRemoteDataSource {
+
+    suspend fun register(request: RegisterRequest): Result<RegisterResponse, DataError.Remote>
+
+    suspend fun login(request: LoginRequest): Result<LoginResponse, DataError.Remote>
+
+}
