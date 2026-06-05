2739f43e1f82546bb303b96e99ff3873d2529ea9
Sendiko <rizkysendiko7@gmail.com>
2026-06-05 21:33:14 +0700

    chore: update imports

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/login/data/dto/LoginResponse.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/login/data/dto/LoginResponse.kt
@@ -1,6 +1,6 @@
 package id.my.sendiko.fintrack.auth.login.data.dto
 
-import id.my.sendiko.fintrack.auth.core.data.UserItem
+import id.my.sendiko.fintrack.auth.core.data.dto.UserItem
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 
