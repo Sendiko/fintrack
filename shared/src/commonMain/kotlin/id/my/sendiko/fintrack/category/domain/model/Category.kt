6ccdb2e2d7e3f84c932e1d2dffe4c38495e80d4b
Sendiko <rizkysendiko7@gmail.com>
2026-06-08 17:54:30 +0700

    refactor: override toString in Category domain model
    - **Category.kt**: Overrode the `toString()` method to return the `name` property, providing a cleaner string representation of the model.

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/domain/model/Category.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/domain/model/Category.kt
@@ -3,4 +3,8 @@ package id.my.sendiko.fintrack.category.domain.model
 data class Category(
     val id: String,
     val name: String,
-)
+) {
+    override fun toString(): String {
+        return name
+    }
+}
