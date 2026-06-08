bbde9b768e9a2aed62ed712fec109f6fb85898da
Sendiko <rizkysendiko7@gmail.com>
2026-06-08 17:55:40 +0700

    feat: add `postTransaction` to `TransactionDataSource`
    This commit adds the ability to send new transaction data to the remote API by updating the transaction data source interface and its implementation.
    
    - **TransactionDataSource.kt**: Added `postTransaction` function to the interface, accepting a `PostTransactionRequest` and returning a `Result` with `PostTransactionResponse`.
    - **TransactionDataSourceImpl.kt**: Implemented the `postTransaction` function using Ktor's `post` method, setting the request body with the provided transaction data.

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSource.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSource.kt
@@ -3,9 +3,13 @@ package id.my.sendiko.fintrack.transaction.core.data.datasource
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
 import id.my.sendiko.fintrack.transaction.core.data.dto.GetTransactionsResponse
+import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionRequest
+import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionResponse
 
 interface TransactionDataSource {
 
     suspend fun getTransactions(): Result<GetTransactionsResponse, DataError.Remote>
+
+    suspend fun postTransaction(request: PostTransactionRequest): Result<PostTransactionResponse, DataError.Remote>
 
 }
--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSourceImpl.kt
@@ -4,8 +4,12 @@ import id.my.sendiko.fintrack.core.network.safeCall
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
 import id.my.sendiko.fintrack.transaction.core.data.dto.GetTransactionsResponse
+import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionRequest
+import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 
 class TransactionDataSourceImpl(
     private val client: HttpClient
@@ -13,6 +17,14 @@ class TransactionDataSourceImpl(
     override suspend fun getTransactions(): Result<GetTransactionsResponse, DataError.Remote> {
         return safeCall<GetTransactionsResponse> {
             client.get("transactions")
+        }
+    }
+
+    override suspend fun postTransaction(request: PostTransactionRequest): Result<PostTransactionResponse, DataError.Remote> {
+        return safeCall<PostTransactionResponse> {
+            client.post("transactions") {
+                setBody(request)
+            }
         }
     }
 }
