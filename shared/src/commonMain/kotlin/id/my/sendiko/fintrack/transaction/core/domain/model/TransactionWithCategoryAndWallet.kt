020535e7ffd1e4ab6bdade6c8127276842aad3fe
Sendiko <rizkysendiko7@gmail.com>
2026-06-08 22:39:28 +0700

    feat: introduce `TransactionWithCategoryAndWallet` domain model
    This commit adds a new domain model to aggregate transaction data with its associated category and wallet information.
    
    - **TransactionWithCategoryAndWallet.kt**: Created a new data class that combines `Transaction`, `Category`, and `Wallet` objects.

---
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/domain/model/TransactionWithCategoryAndWallet.kt
@@ -0,0 +1,10 @@
+package id.my.sendiko.fintrack.transaction.core.domain.model
+
+import id.my.sendiko.fintrack.category.domain.model.Category
+import id.my.sendiko.fintrack.wallet.core.domain.Wallet
+
+data class TransactionWithCategoryAndWallet(
+    val transaction: Transaction,
+    val category: Category,
+    val wallet: Wallet
+)
