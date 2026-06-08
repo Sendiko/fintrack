b991f8eff7596340ea2ea762ec5132bb6443105f
Sendiko <rizkysendiko7@gmail.com>
2026-06-08 07:33:08 +0700

    refactor: remove unused imports across dashboard components
    This commit cleans up the codebase by removing several unused imports in the dashboard's presentation, data, and domain layers.
    
    - **DashboardViewModel.kt**: Removed unused `Category` and `Wallet` imports.
    - **DashboardRepositoryImpl.kt**: Removed unused `GetCategoriesResponse`, `Category`, and `GetTransactionsResponse` imports.
    - **DashboardRepository.kt**: Removed unused `Category` import.

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/core/data/datasource/AuthRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/auth/core/data/datasource/AuthRemoteDataSourceImpl.kt
@@ -1,5 +1,8 @@
 package id.my.sendiko.fintrack.auth.core.data.datasource
 
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.ChangePasswordRequest
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.ChangePasswordResponse
+import id.my.sendiko.fintrack.auth.changepassword.data.dto.SearchUserResponse
 import id.my.sendiko.fintrack.auth.login.data.dto.LoginRequest
 import id.my.sendiko.fintrack.auth.login.data.dto.LoginResponse
 import id.my.sendiko.fintrack.auth.register.data.dto.RegisterRequest
@@ -8,7 +11,10 @@ import id.my.sendiko.fintrack.core.network.safeCall
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 
 class AuthRemoteDataSourceImpl(
@@ -25,6 +31,25 @@ class AuthRemoteDataSourceImpl(
     override suspend fun login(request: LoginRequest): Result<LoginResponse, DataError.Remote> {
         return safeCall<LoginResponse> {
             client.post("login") {
+                setBody(request)
+            }
+        }
+    }
+
+    override suspend fun searchUser(email: String): Result<SearchUserResponse, DataError.Remote> {
+        return safeCall<SearchUserResponse> {
+            client.get("users") {
+                parameter("email", email)
+            }
+        }
+    }
+
+    override suspend fun updatePassword(
+        userId: String,
+        request: ChangePasswordRequest
+    ): Result<ChangePasswordResponse, DataError.Remote> {
+        return safeCall<ChangePasswordResponse> {
+            client.put("user/change-password/$userId") {
                 setBody(request)
             }
         }
