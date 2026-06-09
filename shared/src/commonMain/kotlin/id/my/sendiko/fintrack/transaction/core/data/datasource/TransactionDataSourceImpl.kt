0d2f8fd4af1ccee17642daf78242a1949e04605a
Sendiko <rizkysendiko7@gmail.com>
2026-06-09 12:30:17 +0700

    feat: implement single transaction retrieval and update in TransactionDataSource
    This commit extends the transaction data source to support fetching a single transaction by its ID and updating existing transactions.
    
    - **TransactionDataSource.kt**: Added `getTransaction` and `putTransaction` definitions to the interface.
    - **TransactionDataSourceImpl.kt**: Implemented the new methods using Ktor's `get` and `put` functions, including handling the request body for updates and mapping responses to `GetTransactionResponse` and `PutTransactionResponse`.

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/transaction/core/data/datasource/TransactionDataSourceImpl.kt
@@ -3,12 +3,15 @@ package id.my.sendiko.fintrack.transaction.core.data.datasource
 import id.my.sendiko.fintrack.core.network.safeCall
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
+import id.my.sendiko.fintrack.transaction.core.data.dto.GetTransactionResponse
 import id.my.sendiko.fintrack.transaction.core.data.dto.GetTransactionsResponse
 import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionRequest
 import id.my.sendiko.fintrack.transaction.core.data.dto.PostTransactionResponse
+import id.my.sendiko.fintrack.transaction.core.data.dto.PutTransactionResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 
 class TransactionDataSourceImpl(
@@ -20,9 +23,26 @@ class TransactionDataSourceImpl(
         }
     }
 
+    override suspend fun getTransaction(id: String): Result<GetTransactionResponse, DataError.Remote> {
+        return safeCall<GetTransactionResponse> {
+            client.get("transactions/$id")
+        }
+    }
+
     override suspend fun postTransaction(request: PostTransactionRequest): Result<PostTransactionResponse, DataError.Remote> {
         return safeCall<PostTransactionResponse> {
             client.post("transactions") {
+                setBody(request)
+            }
+        }
+    }
+
+    override suspend fun putTransaction(
+        id: String,
+        request: PostTransactionRequest
+    ): Result<PutTransactionResponse, DataError.Remote> {
+        return safeCall<PutTransactionResponse> {
+            client.put("transactions/$id") {
                 setBody(request)
             }
         }
