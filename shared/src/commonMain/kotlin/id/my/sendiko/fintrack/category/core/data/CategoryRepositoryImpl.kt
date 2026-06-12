9d566cbfa299e9cd6c57a1df844dac288f021ab7
Sendiko <rizkysendiko7@gmail.com>
2026-06-12 10:56:50 +0700

    chore: adjust dto to domain conversion

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/data/CategoryRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/data/CategoryRepositoryImpl.kt
@@ -2,16 +2,16 @@ package id.my.sendiko.fintrack.category.core.data
 
 import id.my.sendiko.fintrack.category.core.data.datasource.CategoryDataSource
 import id.my.sendiko.fintrack.category.core.domain.CategoryRepository
-import id.my.sendiko.fintrack.category.core.domain.model.Category
+import id.my.sendiko.fintrack.category.core.domain.model.CategoryWithTransactions
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
 
 class CategoryRepositoryImpl(
     private val dataSource: CategoryDataSource
 ) : CategoryRepository {
-    override suspend fun getCategories(): Result<List<Category>, DataError.Remote> {
+    override suspend fun getCategories(): Result<List<CategoryWithTransactions>, DataError.Remote> {
         return when (val response = dataSource.getCategories()) {
-            is Result.Success -> Result.Success(response.data.categories.map { it.toDomain() })
+            is Result.Success -> Result.Success(response.data.categories.map { it.toDomainWithTransaction() })
             is Result.Error -> Result.Error(response.error)
         }
     }
--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/domain/CategoryRepository.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/domain/CategoryRepository.kt
@@ -1,11 +1,11 @@
 package id.my.sendiko.fintrack.category.core.domain
 
-import id.my.sendiko.fintrack.category.core.domain.model.Category
+import id.my.sendiko.fintrack.category.core.domain.model.CategoryWithTransactions
 import id.my.sendiko.fintrack.core.network.utils.DataError
 import id.my.sendiko.fintrack.core.network.utils.Result
 
 interface CategoryRepository {
 
-    suspend fun getCategories(): Result<List<Category>, DataError.Remote>
+    suspend fun getCategories(): Result<List<CategoryWithTransactions>, DataError.Remote>
 
 }
