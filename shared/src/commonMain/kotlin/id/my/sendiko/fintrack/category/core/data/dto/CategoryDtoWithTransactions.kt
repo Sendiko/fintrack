3052a9fb9134f12f126c443cbd1161ebbf02d128
Sendiko <rizkysendiko7@gmail.com>
2026-06-12 10:56:11 +0700

    chore: format code

--- shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/data/dto/CategoryDtoWithTransactions.kt
+++ shared/src/commonMain/kotlin/id/my/sendiko/fintrack/category/core/data/dto/CategoryDtoWithTransactions.kt
@@ -3,36 +3,36 @@ package id.my.sendiko.fintrack.category.core.data.dto
 import id.my.sendiko.fintrack.category.core.domain.model.Category
 import id.my.sendiko.fintrack.category.core.domain.model.CategoryWithTransactions
 import id.my.sendiko.fintrack.transaction.core.data.dto.TransactionsDto
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class CategoryDtoWithTransactions(
 
-	@SerialName("createdAt")
-	val createdAt: String,
+    @SerialName("createdAt")
+    val createdAt: String,
 
-	@SerialName("name")
-	val name: String,
+    @SerialName("name")
+    val name: String,
 
-	@SerialName("id")
-	val id: String,
+    @SerialName("id")
+    val id: String,
 
-	@SerialName("transactions")
-	val transactions: List<TransactionsDto>,
+    @SerialName("transactions")
+    val transactions: List<TransactionsDto>,
 
-	@SerialName("userId")
-	val userId: String,
+    @SerialName("userId")
+    val userId: String,
 
-	@SerialName("updatedAt")
-	val updatedAt: String
+    @SerialName("updatedAt")
+    val updatedAt: String
 ) {
-	fun toDomain() = Category(
+    fun toDomain() = Category(
         id = id,
         name = name
     )
 
-	fun toDomainWithTransaction() = CategoryWithTransactions(
+    fun toDomainWithTransaction() = CategoryWithTransactions(
         category = this.toDomain(),
         transactions = this.transactions.map { it.toDomain() }
     )
